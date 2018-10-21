3cbfec591d3f7cf8d7b81ddcd99fa177425e745b
Jerry Han <jayh511ster@gmail.com>
2018-10-21 17:06:10 -0500

    adding files to git

---
+++ mainfile.py
@@ -0,0 +1,23 @@
+#-----------------------------------------
+# mainfile.py
+# creating first flask application
+#-----------------------------------------
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+ return render_template('first_page.html')
+#@app.route('')
+
+#def teams():
+ #return render_template('team.html')
+ #pass
+#def weeks():
+ #return render_template('weeks.html') 
+ #pass
+if __name__ == "__main__":
+ app.run()
+#----------------------------------------
+# end of main2.py
+#----------------------------------------- 
