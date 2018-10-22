bf55808fe8a2f87c3f150209d0c009c4a8fdb3f8
Amber Tucker <tuckera@cs.utexas.edu>
2018-10-21 22:03:17 -0500

    Added players.html, updated mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -7,15 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
- return render_template('first_page.html')
+ return render_template('splash.html')
+
+@app.route('/players')
+def players():
+  return render_template('players.html')
 
 @app.route('/teams')
 def teams():
  return render_template('teams.html')
 
+@app.route('/weeks')
+def weeks():
+  return render_template('first_page.html')
+
 #def weeks():
- #return render_template('weeks.html')
- #pass
+# return render_template('weeks.html')
+# pass
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
