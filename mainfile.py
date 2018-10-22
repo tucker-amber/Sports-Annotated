a95eb3eae82088a02791c13431646c1cb8d98859
Dan Tran <Dan@wireless-10-145-22-90.public.utexas.edu>
2018-10-21 19:32:40 -0500

    add teams page

--- mainfile.py
+++ mainfile.py
@@ -4,20 +4,20 @@
 #-----------------------------------------
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def index():
  return render_template('first_page.html')
-#@app.route('')
 
-#def teams():
- #return render_template('team.html')
- #pass
+@app.route('/teams')
+def teams():
+ return render_template('teams.html')
+
 #def weeks():
- #return render_template('weeks.html') 
+ #return render_template('weeks.html')
  #pass
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
 # end of main2.py
-#----------------------------------------- 
+#-----------------------------------------
