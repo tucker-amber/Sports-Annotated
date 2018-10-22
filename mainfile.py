324825dcc1b56de996a8e6f05cc3bcfa4a79967f
Jerry Han <jayh511ster@gmail.com>
2018-10-22 10:31:43 -0500

    changes

--- mainfile.py
+++ mainfile.py
@@ -8,18 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
  return render_template('splash.html')
-<<<<<<< HEAD
 
- #@app.route('')
 
-def teams():
- #return render_template('team.html')
-  pass
-@app.route('/weeks/')
+@app.route('/weeks')
 def weeks():
  return render_template('weeks.html') 
-
-=======
 
 @app.route('/players')
 def players():
@@ -29,14 +22,6 @@ def players():
 def teams():
  return render_template('teams.html')
 
-@app.route('/weeks')
-def weeks():
-  return render_template('weeks.html')
-
-#def weeks():
-# return render_template('weeks.html')
-# pass
->>>>>>> bf55808fe8a2f87c3f150209d0c009c4a8fdb3f8
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
