8d4594a8ea59f4f932aedd7e96f15528e63cf054
Jerry Han <jayh511ster@gmail.com>
2018-10-22 03:43:31 -0500

    adding weeks.html

--- mainfile.py
+++ mainfile.py
@@ -4,18 +4,20 @@
 #-----------------------------------------
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
-
 def index():
- return render_template('first_page.html')
-#@app.route('')
+ return render_template('splash.html')
 
-#def teams():
+ #@app.route('')
+
+def teams():
  #return render_template('team.html')
- #pass
-#def weeks():
- #return render_template('weeks.html') 
- #pass
+  pass
+@app.route('/weeks/')
+def weeks():
+ return render_template('weeks.html') 
+
 if __name__ == "__main__":
  app.run()
 #----------------------------------------
