632068bffa7687bdcf2cd8801651a3d45cd069ab
Amber Tucker <tuckera@cs.utexas.edu>
2018-10-23 23:04:54 -0500

    linked players.html to player pages (brady.html, hoyer.html, barner.html) and added paths in mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -18,6 +18,21 @@ def weeks():
 def players():
   return render_template('players.html')
 
+# Navigates to ind player's page
+@app.route('/hoyer')
+def hoyer():
+  return render_template('hoyer.html')
+
+# Navigates to ind player's page
+@app.route('/brady')
+def brady():
+  return render_template('brady.html')
+
+# Navigates to ind player's page
+@app.route('/barner')
+def barner():
+  return render_template('barner.html')
+
 @app.route('/teams')
 def teams():
   return render_template('teams.html')
