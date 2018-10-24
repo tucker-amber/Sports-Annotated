247096a6321acd4fb2c342f2ad1c02373d266546
Jerry Han <jayh511ster@gmail.com>
2018-10-24 10:32:38 -0500

    Adding Games1.html, game2.html, game.html3

--- mainfile.py
+++ mainfile.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def index():
   return render_template('splash.html')
 
-
 @app.route('/weeks')
 def weeks():
   return render_template('weeks.html') 
@@ -40,6 +39,18 @@ def teams():
 @app.route('/about')
 def about():
   return render_template('about.html')
+  
+@app.route('/game1')
+def game1():
+  return render_template('game1.html')
+  
+@app.route('/game2')
+def game2():
+  return render_template('game2.html')
+  
+@app.route('/game3')
+def game3():
+  return render_template('game3.html')
 
 if __name__ == "__main__":
  app.run()
