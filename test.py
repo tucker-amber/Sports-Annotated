585613ed39012ef632c9461f68276f6261ad7f89
Jerry Han <jayh511ster@gmail.com>
2018-11-07 03:15:24 -0600

    changed player to players on test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from models import db, Nfl
 
 class DBTestCases(unittest.TestCase):
 	def test_source_insert_1(self):
-		s = Nfl(id='1', jersey_num = 83, player = "Dwayne Allen", age = 28, pos = "TE", team = 'Patriots')
+		s = Nfl(id='1', jersey_num = 83, players = "Dwayne Allen", age = 28, pos = "TE", team = 'Patriots')
 		db.session.add(s)
 		db.session.commit()
 		r = db.session.query(Nfl).filter_by(id = '1').one()
