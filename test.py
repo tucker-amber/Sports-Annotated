fb6d5bc9e465b4cc3f6ef84a5ab88cc06e11bb40
Jerry Han <jayh511ster@gmail.com>
2018-11-07 03:13:18 -0600

    made changes to test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import os
 import sys
 import unittest
-from models import db, Book
+from models import db, Nfl
 
 class DBTestCases(unittest.TestCase):
 	def test_source_insert_1(self):
-		s = Book(id='20', title = 'C++')
+		s = Nfl(id='1', jersey_num = 83, player = "Dwayne Allen", age = 28, pos = "TE", team = 'Patriots')
 		db.session.add(s)
 		db.session.commit()
-		r = db.session.query(Book).filter_by(id = '20').one()
-		self.assertEqual(str(r.id), '20')
+		r = db.session.query(Nfl).filter_by(id = '1').one()
+		self.assertEqual(str(r.id), '1')
 
-		db.session.query(Book).filter_by(id = '20').delete()
+		db.session.query(Nfl).filter_by(id = '1').delete()
 		db.session.commit()
 
 if __name__ == '__main__':
