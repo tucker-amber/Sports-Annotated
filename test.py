643a5de08481d08acd0d15cc9b99666f62745c16
Jerry Han <jayh511ster@gmail.com>
2018-11-09 02:26:00 -0600

    changes to unit test

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import os
 import sys
 import unittest
-from models import db, Nfl
+from models import db, Player, Teams, Weeks
 
 class DBTestCases(unittest.TestCase):
 	def test_source_insert_1(self):
-		s = Nfl(id='1', jersey_num = 83, players = "Dwayne Allen", age = 28, pos = "TE", team = 'Patriots')
+		s = Player(id ='1', jersey_num = 83, players = "Dwayne Allen", age = 28, pos = "TE", team = 'patriots')
 		db.session.add(s)
 		db.session.commit()
-		r = db.session.query(Nfl).filter_by(id = '1').one()
+		r = db.session.query(Player).filter_by(id = '1').one()
 		self.assertEqual(str(r.id), '1')
 
-		db.session.query(Nfl).filter_by(id = '1').delete()
+		db.session.query(Player).filter_by(id = '1').delete()
 		db.session.commit()
 
 if __name__ == '__main__':
